b48c4fd230dfec98a451906eb83b9b38ae900797
Gregory Ross <gregross.dev@gmail.com>
2023-06-25 23:41:40 -0400

    fix cart

--- src/views/Shopping/CartView.vue
+++ src/views/Shopping/CartView.vue
@@ -60,9 +60,6 @@ function refreshPage(id) {
 }
 
 // paypal
-const paypalCreateOrder = window.firebase.functions().httpsCallable("paypalCreateOrder");
-const paypalHandleOrder = window.firebase.functions().httpsCallable("paypalHandleOrder");
-
 const paypal = ref(null);
 let  paidFor = ref(false);
 let loaded = ref(false);
